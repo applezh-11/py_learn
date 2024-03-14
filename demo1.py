bcaf4a2008aaad44a4b3e995c8459d1ef063470e
applezh-11 <163382656+applezh-11@users.noreply.github.com>
2024-03-14 20:23:18 +0800

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,14 @@
+# -*- codeing =utf-8 -*-
+# @Time :2024/1/25 9:42
+# @Autor : 张浩
+# @File :demo1.py
+# @Software: PyCharm
+#这是我的第一个python程序
+print("hello,world") #这是单行注释
+
+'''
+这是第一行注释
+这是第二行注释
+'''
+print("python")
+
